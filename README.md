c0dda22c42ccb35b183ff9f7f904b88a4cf50995
Nathan Osman <admin@quickmediasolutions.com>
2013-07-23 11:23:46 -0700

    Added ability for script to update at regular intervals and for output directory to be changed.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-## Ubuntu-Edge-Image
+## Ubuntu Edge Image
 
-This handy Python script generates an image displaying the percentage of funds raised for the [Ubuntu Edge crowdfunding project](http://www.indiegogo.com/projects/ubuntu-edge). The entire script is written in Python and requires the following Ubuntu packages:
+This handy Python script generates an image displaying the percentage of funds raised for the [Ubuntu Edge crowdfunding project](http://www.indiegogo.com/projects/ubuntu-edge). The entire script is written in Python and requires that the following Ubuntu packages be installed:
 
  - `python-lxml`
  - `python-imaging`
@@ -9,4 +9,12 @@ The script is executable and can be run simply with:
 
     ./update.py
 
-This will generate a file named `percentage.png` in the current directory.
+By default, this will generate a file named `percentage.png` in the current directory. If you wish to have the image stored somewhere else, you can specify the directory via the `--directory` argument.
+
+### Regular Intervals
+
+The script can be set to run continuously, updating the image at regular intervals. To do this, simply invoke the script as follows:
+
+    ./update.py --interval 30
+
+This will cause the script to update the image every 30 minutes.
--- update.py
+++ update.py
@@ -1,39 +1,67 @@
 #!/usr/bin/env python
 
+from argparse import ArgumentParser
 from json import dumps
-from lxml import html
 from os import path
 from re import match
 from time import sleep
 from urllib2 import urlopen
 
+from lxml import html
 from PIL import Image, ImageDraw, ImageFont
 
-def retrieve_percentage():
+class ImageUpdater:
     '''
-    Retrieves the latest value as a percentage.
-    Throws all manner of exceptions due to network problems,
-    HTML parsing errors, invalid list indices, etc.
+    Updates the image displaying percentage of completion.
     '''
-    page = urlopen('http://www.indiegogo.com/projects/ubuntu-edge').read()
-    element = html.fromstring(page).find_class('progress')[0]
-    progress = float(match(r'[\d.]+', element.text).group(0))
-    return '%.f%%' % (progress * 100)
 
-def generate_image(directory):
-    '''
-    Generates a PNG image displaying the provided percentage.
-    The image combines the background design and bright green text.
-    '''
-    img = Image.open('bg.png')
-    draw = ImageDraw.Draw(img)
-    font = ImageFont.truetype('Ubuntu-B.ttf', 30)
-    percentage = retrieve_percentage()
-    size = draw.textsize(percentage, font=font)
-    draw.text((50 - size[0] / 2, 40 - size[1] / 2), percentage,
-              fill=(0, 192, 0,), font=font)
-    img.save(path.join(directory, 'percentage.png'), 'PNG')
+    def retrieve_percentage(self):
+        '''
+        Retrieves the latest value as a percentage.
+        Throws all manner of exceptions due to network problems,
+        HTML parsing errors, invalid list indices, etc.
+        '''
+        page = urlopen('http://www.indiegogo.com/projects/ubuntu-edge').read()
+        element = html.fromstring(page).find_class('progress')[0]
+        progress = float(match(r'[\d.]+', element.text).group(0))
+        return '%.f%%' % (progress * 100)
 
-# Note that exceptions are NOT handled here
+    def generate_image(self, directory):
+        '''
+        Generates a PNG image displaying the provided percentage.
+        The image combines the background design and bright green text.
+        '''
+        img = Image.open('bg.png')
+        draw = ImageDraw.Draw(img)
+        font = ImageFont.truetype('Ubuntu-B.ttf', 30)
+        percentage = self.retrieve_percentage()
+        size = draw.textsize(percentage, font=font)
+        draw.text((50 - size[0] / 2, 40 - size[1] / 2), percentage,
+                fill=(0, 192, 0,), font=font)
+        img.save(path.join(directory, 'percentage.png'), 'PNG')
+    
+    def run(self, args):
+        '''
+        Runs the application according to the interval argument.
+        '''
+        while True:
+            try:
+                self.generate_image(args.directory)
+            except Exception, e:
+                print e
+            if not args.interval:
+                break
+            sleep(60 * args.interval)
+
 if __name__ == '__main__':
-    generate_image('.')
+    parser = ArgumentParser(description='Updates percentage image')
+    parser.add_argument('--directory',
+                        type=str,
+                        default='.',
+                        help='directory to create the image in')
+    parser.add_argument('--interval',
+                        metavar='MINUTES',
+                        type=int,
+                        default=0,
+                        help='number of minutes between updates (0 to run once)')
+    ImageUpdater().run(parser.parse_args())
